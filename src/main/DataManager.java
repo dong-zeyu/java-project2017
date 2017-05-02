027a13552add18913c43826fa11336eaae9d9b16
Dongzy <dzy201415@gmail.com>
2017-05-02 17:55:37 +0800

    change some field name and add arrenge exception

--- src/main/DataManager.java
+++ src/main/DataManager.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,6 +20,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import flight.City;
 import flight.Flight;
@@ -82,14 +84,11 @@ public class DataManager {
 	
 	public DataManager() {
 		try {
-			init(true);
-		} catch (IOException | ParserConfigurationException e) {
-			try {
-				System.out.println("Read file error!");
-				init(false);
-			} catch (IOException | ParserConfigurationException e1) {}
+			init();
+		} catch (IOException e) {
+			System.out.println("Read/write data error!");
+			System.exit(-1);
 		}
-		
 	}
 	
 	public Flight getFlightByID(int flightID) {
@@ -107,128 +106,137 @@ public class DataManager {
 		return null;
 	}
 	
-	public void saveData() throws IOException, ParserConfigurationException {
-		Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-		Element root = document.createElement("root");
-		document.appendChild(root);
-		Element user = document.createElement("user");
-		Element flight = document.createElement("flight");
-		Element city = document.createElement("city");
-		root.appendChild(user);
-		root.appendChild(city);
-		root.appendChild(flight);
-		//Users
-		Element egga;
-		Element eggp;
-		egga = document.createElement("admin");
-		egga.appendChild(document.createElement("username"));
-		egga.appendChild(document.createElement("passhash"));
-		eggp = document.createElement("passenger");
-		eggp.appendChild(document.createElement("username"));
-		eggp.appendChild(document.createElement("passhash"));
-		eggp.appendChild(document.createElement("idnumber"));
-		eggp.appendChild(document.createElement("order"));
-		for (User user0 : users) {
-			if (user0 instanceof Admin) {
-				Admin admin = (Admin) user0;
-				NodeList tmp = egga.cloneNode(true).getChildNodes();
-				tmp.item(0).appendChild(document.createTextNode(admin.getUserName()));
-				tmp.item(1).appendChild(document.createTextNode(admin.getPassHash()));
-				user.appendChild(tmp.item(0).getParentNode());				
-			} else if (user0 instanceof Passenger) {
-				Passenger passenger = (Passenger) user0;
-				NodeList tmp = eggp.cloneNode(true).getChildNodes();
-				tmp.item(0).appendChild(document.createTextNode(passenger.getUserName()));
-				tmp.item(1).appendChild(document.createTextNode(passenger.getPassHash()));
-				tmp.item(2).appendChild(document.createTextNode(passenger.getIdentityID()));
-				Element otmp = (Element) tmp.item(4);
-				Element oegg = document.createElement("item");
-				oegg.appendChild(document.createElement("flightid"));
-				oegg.appendChild(document.createElement("seat"));
-				oegg.appendChild(document.createElement("date"));
-				oegg.appendChild(document.createElement("status"));
-				for (Order order : passenger.getOrderList()) {
-					NodeList item = oegg.cloneNode(true).getChildNodes();
-					item.item(0).appendChild(document.createTextNode(String.valueOf(order.getFlight().getFlightID())));
-					item.item(1).appendChild(document.createTextNode(String.valueOf(order.getSeat())));
-					item.item(2).appendChild(document.createTextNode(String.valueOf(order.getCreatDate().getTime())));
-					item.item(3).appendChild(document.createTextNode(String.valueOf(order.getStatus().name())));
-					otmp.appendChild(item.item(0).getParentNode());
+	public void saveData() throws FileNotFoundException {
+		Document document;
+		try {
+			document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+			Element root = document.createElement("root");
+			document.appendChild(root);
+			Element euser = document.createElement("user");
+			Element eflight = document.createElement("flight");
+			Element ecity = document.createElement("city");
+			root.appendChild(euser);
+			root.appendChild(ecity);
+			root.appendChild(eflight);
+			//Users
+			Element adminEgg;
+			Element passengerEgg;
+			adminEgg = document.createElement("admin");
+			adminEgg.appendChild(document.createElement("username"));
+			adminEgg.appendChild(document.createElement("passhash"));
+			passengerEgg = document.createElement("passenger");
+			passengerEgg.appendChild(document.createElement("username"));
+			passengerEgg.appendChild(document.createElement("passhash"));
+			passengerEgg.appendChild(document.createElement("idnumber"));
+			passengerEgg.appendChild(document.createElement("order"));
+			for (User user : users) {
+				if (user instanceof Admin) {
+					Admin admin = (Admin) user;
+					NodeList nodes = adminEgg.cloneNode(true).getChildNodes();
+					nodes.item(0).appendChild(document.createTextNode(admin.getUserName()));
+					nodes.item(1).appendChild(document.createTextNode(admin.getPassHash()));
+					euser.appendChild(nodes.item(0).getParentNode());				
+				} else if (user instanceof Passenger) {
+					Passenger passenger = (Passenger) user;
+					NodeList nodes = passengerEgg.cloneNode(true).getChildNodes();
+					nodes.item(0).appendChild(document.createTextNode(passenger.getUserName()));
+					nodes.item(1).appendChild(document.createTextNode(passenger.getPassHash()));
+					nodes.item(2).appendChild(document.createTextNode(passenger.getIdentityID()));
+					Element orderEgg = document.createElement("item");
+					orderEgg.appendChild(document.createElement("flightid"));
+					orderEgg.appendChild(document.createElement("seat"));
+					orderEgg.appendChild(document.createElement("date"));
+					orderEgg.appendChild(document.createElement("status"));
+					for (Order order : passenger.getOrderList()) {
+						NodeList item = orderEgg.cloneNode(true).getChildNodes();
+						item.item(0).appendChild(document.createTextNode(String.valueOf(order.getFlight().getFlightID())));
+						item.item(1).appendChild(document.createTextNode(String.valueOf(order.getSeat())));
+						item.item(2).appendChild(document.createTextNode(String.valueOf(order.getCreatDate().getTime())));
+						item.item(3).appendChild(document.createTextNode(String.valueOf(order.getStatus().name())));
+						nodes.item(4).appendChild(item.item(0).getParentNode());
+					}
+					euser.appendChild(nodes.item(0).getParentNode());
 				}
-				user.appendChild(tmp.item(0).getParentNode());
 			}
+			adminEgg = null;
+			passengerEgg = null;
+			//flight
+			Element flightEgg;
+			flightEgg = document.createElement("item");
+			flightEgg.appendChild(document.createElement("flightname"));
+			flightEgg.appendChild(document.createElement("starttime"));
+			flightEgg.appendChild(document.createElement("arrivetime"));
+			flightEgg.appendChild(document.createElement("startcity"));
+			flightEgg.appendChild(document.createElement("arrivecity"));
+			flightEgg.appendChild(document.createElement("price"));
+			flightEgg.appendChild(document.createElement("seatcapacity"));
+			flightEgg.appendChild(document.createElement("status"));
+			for (Flight f : flights) {
+				NodeList nodes = flightEgg.cloneNode(true).getChildNodes();
+				nodes.item(0).appendChild(document.createTextNode(f.getFlightName()));
+				nodes.item(1).appendChild(document.createTextNode(String.valueOf(f.getStartTime().getTime())));
+				nodes.item(2).appendChild(document.createTextNode(String.valueOf(f.getArriveTime().getTime())));
+				nodes.item(3).appendChild(document.createTextNode(f.getStartCity().getCityName()));
+				nodes.item(4).appendChild(document.createTextNode(f.getArriveCity().getCityName()));
+				nodes.item(5).appendChild(document.createTextNode(String.valueOf(f.getPrice())));
+				nodes.item(6).appendChild(document.createTextNode(String.valueOf(f.getSeatCapacity())));
+				nodes.item(7).appendChild(document.createTextNode(f.getFlightStatus().name()));
+				eflight.appendChild(nodes.item(0).getParentNode());
+			}
+			flightEgg = null;
+			//city
+			for (City c : cities) {
+				ecity.appendChild(document.createElement("item"))
+					.appendChild(document.createTextNode(c.getCityName()));
+			}
+			saveDocument(document);
+		} catch (ParserConfigurationException e) {
 		}
-		//flight
-		Element egg;
-		egg = document.createElement("item");
-		egg.appendChild(document.createElement("flightname"));
-		egg.appendChild(document.createElement("starttime"));
-		egg.appendChild(document.createElement("arrivetime"));
-		egg.appendChild(document.createElement("startcity"));
-		egg.appendChild(document.createElement("arrivecity"));
-		egg.appendChild(document.createElement("price"));
-		egg.appendChild(document.createElement("seatcapacity"));
-		egg.appendChild(document.createElement("status"));
-		for (Flight f : flights) {
-			NodeList tmp = egg.cloneNode(true).getChildNodes();
-			tmp.item(0).appendChild(document.createTextNode(f.getFlightName()));
-			tmp.item(1).appendChild(document.createTextNode(String.valueOf(f.getStartTime().getTime())));
-			tmp.item(2).appendChild(document.createTextNode(String.valueOf(f.getArriveTime().getTime())));
-			tmp.item(3).appendChild(document.createTextNode(f.getStartCity().getCityName()));
-			tmp.item(4).appendChild(document.createTextNode(f.getArriveCity().getCityName()));
-			tmp.item(5).appendChild(document.createTextNode(String.valueOf(f.getPrice())));
-			tmp.item(6).appendChild(document.createTextNode(String.valueOf(f.getSeatCapacity())));
-			tmp.item(7).appendChild(document.createTextNode(f.getFlightStatus().name()));
-			flight.appendChild(tmp.item(0).getParentNode());
-		}
-		//city
-		for (City c : cities) {
-			city.appendChild(document.createElement("item"))
-				.appendChild(document.createTextNode(c.getCityName()));
-		}
-		saveDocument(document);
 	}
 
-	private void init(boolean isCreate) throws IOException, ParserConfigurationException {
-		file = new File(filename);
-		if (!file.exists()) {
-			if (isCreate) {				
+	private void init() throws IOException {
+		try {
+			file = new File(filename);
+			if (!file.exists()) {	
 				file.createNewFile();
-			}
-			Admin admin = new Admin("Admin", "admin");
-			users.add(admin);
-			City shenz = new City("Shenzhen");
-			City beij = new City("Beijing");
-			City zhenz = new City("Zhengzhou");
-			cities.add(zhenz);
-			cities.add(beij);
-			cities.add(shenz);
-			Flight flight1 = new Flight("A001",
-					Flight.calendar(2017, 4, 1, 9, 30, 0),
-					Flight.calendar(2017, 4, 1, 10, 40, 0), shenz, beij, 1200, 120);
-			Flight flight2 = new Flight("A002",
-					Flight.calendar(2017, 5, 2, 9, 12, 0),
-					Flight.calendar(2017, 5, 2, 10, 42, 0), beij, shenz, 1200, 120);
-			Flight flight3 = new Flight("A003",
-					Flight.calendar(2017, 3, 3, 16, 12, 00),
-					Flight.calendar(2017, 3, 3, 16, 52, 00), zhenz, shenz, 1200, 120);
-			flights.add(flight1);
-			flights.add(flight2);
-			flights.add(flight3);
-			// TODO(Zhu) finish the remain 15 flight
-			if (isCreate) {
+				Admin admin = new Admin("Admin", "admin");
+				users.add(admin);
+				City shenz = new City("Shenzhen");
+				City beij = new City("Beijing");
+				City zhenz = new City("Zhengzhou");
+				cities.add(zhenz);
+				cities.add(beij);
+				cities.add(shenz);
+				Flight flight1 = new Flight("A001",
+						Flight.calendar(2017, 4, 1, 9, 30, 0),
+						Flight.calendar(2017, 4, 1, 10, 40, 0), shenz, beij, 1200, 120);
+				Flight flight2 = new Flight("A002",
+						Flight.calendar(2017, 5, 2, 9, 12, 0),
+						Flight.calendar(2017, 5, 2, 10, 42, 0), beij, shenz, 1200, 120);
+				Flight flight3 = new Flight("A003",
+						Flight.calendar(2017, 3, 3, 16, 12, 00),
+						Flight.calendar(2017, 3, 3, 16, 52, 00), zhenz, shenz, 1200, 120);
+				flights.add(flight1);
+				flights.add(flight2);
+				flights.add(flight3);
 				saveData();
+			} else {
+					readData();
 			}
-		} else {
-			readData();
+		} catch (FileNotFoundException | SAXException e) {
+			file.delete();
+			init();
 		}
 	}
 
-	private void readData() {
-		
+	private void readData() throws SAXException, FileNotFoundException, IOException {
+		try {
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new FileInputStream(file));
+		} catch (ParserConfigurationException e) {
+		}
 	}
 	
-	private void saveDocument(Node document) {
+	private void saveDocument(Node document) throws FileNotFoundException {
         try {
         	Transformer transformer = TransformerFactory.newInstance().newTransformer();
         	transformer.setOutputProperty(OutputKeys.INDENT, "yes");
@@ -238,7 +246,6 @@ public class DataManager {
         	result.setOutputStream(new FileOutputStream(file));
 			transformer.transform(source, result);
 		} catch (TransformerException e) {
-		} catch (FileNotFoundException e) {
 		}
 	}
 		
