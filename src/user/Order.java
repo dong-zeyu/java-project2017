20bec24e151882d8a714fd327ae407d50b760dc3
Dongzy <dzy201415@gmail.com>
2017-05-03 09:09:53 +0800

    add comment to peng's work

--- src/user/Order.java
+++ src/user/Order.java
@@ -43,17 +43,16 @@ public class Order {
 
 	public void printOrder() throws StatusUnavailableException {
 		// TODO(Peng) printOrder
-		
-			if (status==OrderStatus.PAID) {
-				System.out.println("Passager :"+getPassager());
-				System.out.println("Your Seat :"+getSeat());
-				System.out.println("Your Flight :"+getFlight());
-				System.out.println("Create date :"+getCreatDate());
-				System.out.println("OrderStatus :"+getStatus());
-		    	
-		    }
-			else {
-				 throw new StatusUnavailableException();
-			}
+		if (status==OrderStatus.PAID) {// XXX(Peng) Do you think if the order is unpaid, it can never be printed?
+			System.out.println("Passager :"+getPassager());
+			System.out.println("Your Seat :"+getSeat());
+			System.out.println("Your Flight :"+getFlight());
+			System.out.println("Create date :"+getCreatDate());
+			System.out.println("OrderStatus :"+getStatus());
+	    	
+	    }
+		else {
+			 throw new StatusUnavailableException();
 		}
+	}
 }
