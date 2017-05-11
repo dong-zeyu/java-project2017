028611b2ddd659a8244cabca2e1af8ee3daa9c53
Dongzy <dzy201415@gmail.com>
2017-05-11 23:01:42 +0800

    finish changeFlight

--- src/main/MainServer.java
+++ src/main/MainServer.java
@@ -1,9 +1,6 @@
 package main;
 
-import java.text.ParseException;
 import java.util.Date;
-
-import org.omg.CosNaming.NamingContextExtOperations;
 
 import exceptions.PermissionDeniedException;
 import exceptions.StatusUnavailableException;
@@ -82,9 +79,43 @@ public class MainServer {
 	}
 	
 	public boolean changeFlight(String flightName, Date startTime, Date arriveTime, int startCityID, int arriveCityID,
-			int price, int seatCapacity, int flightId) throws PermissionDeniedException { // false when error cityID
+			int price, int seatCapacity, int flightID) throws PermissionDeniedException, StatusUnavailableException { // false when error cityID
 		if (isLogin && isAdmin){
-			return false;
+			Flight flight = dataManager.getFlightByID(flightID);
+			if (flight != null) {
+				if (startCityID != -1) {
+					City startCity = dataManager.getCityByID(startCityID);
+					if (startCity == null) {
+						return false;
+					} 
+					flight.setStartCity(startCity);
+				}
+				if (startCityID != -1) {
+					City arriveCity = dataManager.getCityByID(arriveCityID);
+					if (arriveCity == null) {
+						return false;
+					} 
+					flight.setStartCity(arriveCity);
+				}
+				if (flightName != null) {
+					flight.setFlightName(flightName);
+				}
+				if (startTime != null) {
+					flight.setStartTime(startTime);
+				}
+				if (arriveTime != null) {
+					flight.setArriveTime(arriveTime);
+				}
+				if (seatCapacity != -1) {
+					flight.setSeatCapacity(seatCapacity);
+				}
+				if (price != -1) {
+					flight.setPrice(price);
+				}
+				return true;
+			} else {
+				return false;
+			}
 		} else {
 			throw new PermissionDeniedException();
 		}
